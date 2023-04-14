361c29bae2ee67de369379330b2459e38d370ad8
Sumanth3028 <sumanthakula05@gmail.com>
2023-04-14 12:37:31 +0530

    adding cart

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import uiSlice from "./cart-slice";
+import uiSlice from "./ui-slice";
+
+import cartSlice from "./cart-slice";
 
 const store = configureStore({
-    reducer:{ui:uiSlice.reducer}
+    reducer:{ui:uiSlice.reducer,cart:cartSlice.reducer}
 });
 
 
